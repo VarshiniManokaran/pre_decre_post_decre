fb23e7bde4d64cdaea32ad79ec2e2977befcd946
VarshiniManokaran <varshini6678@gmail.com>
2024-08-06 16:33:22 +0530

    Add files via upload

---
+++ pre_decre_post_decre.java
@@ -0,0 +1,17 @@
+
+
+
+public class pre_decre_post_decre {
+    public static void main(String[] args) {
+        
+    
+    int abc=344;
+    int cba=667;
+    int ccc=789;
+    int vvv=864;
+    int ggg=321;
+    int numbers=--vvv - ccc-- - cba-- - --abc ;
+    System.out.println(numbers);
+    
+}
+}
